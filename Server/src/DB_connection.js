121276a3198cfef08d66bcc502ef600d6603b15a
Solsantacruz <solsantacruz69@gmail.com>
2023-10-19 16:55:18 +0200

    cambios en db

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,35 +1,51 @@
 require('dotenv').config();
+require('pg');
 const { Sequelize } = require('sequelize');
 
-const DB_USER = process.env.DB_USER || 'postgres';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'summeror2020';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 5432;
-const DB_NAME = process.env.DB_NAME || 'rickandmorty';
-const SECURE = process.env.SECURE || false;
+const { DB_DEPLOY } = process.env;
+
+// const DB_USER = process.env.DB_USER || 'postgres';
+// const DB_PASSWORD = process.env.DB_PASSWORD || 'summeror2020';
+// const DB_HOST = process.env.DB_HOST || 'localhost';
+// const DB_PORT = process.env.DB_PORT || 5432;
+// const DB_NAME = process.env.DB_NAME || 'rickandmorty';
+// const SECURE = process.env.SECURE || false;
 
 const UserModel = require('../src/models/User');
 const FavoriteModel = require('../src/models/Favorite');
 
-let strConn = '';
-if (SECURE) {
-   // conexión segura (para BDD remota):
-   strConn = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`;
-} else {
-   strConn = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-}
+// let strConn = '';
+// if (SECURE) {
+//    // conexión segura (para BDD remota):
+//    strConn = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`;
+// } else {
+//    strConn = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+// }
+
+// const database = new Sequelize(
+//    strConn,
+//    { logging: false, native: false }
+// );
 
 const database = new Sequelize(
-   strConn,
-   { logging: false, native: false }
-);
+   DB_DEPLOY,
+   { logging: false,
+     native: false,
+     dialectOptions: {
+       ssl: {
+         require: true,
+         rejectUnauthorized: false, // Solo para desarrollo local. En producción, configúralo correctamente.
+       },
+     },
+   }
+ );
 
 UserModel(database);
 FavoriteModel(database);
 // Relacionar modelos:
 const { User, Favorite } = database.models;
-//User.belongsToMany(Favorite, { through: "user_favorite" });
-//Favorite.belongsToMany(User, { through: "user_favorite" });
+User.belongsToMany(Favorite, { through: "user_favorite" });
+Favorite.belongsToMany(User, { through: "user_favorite" });
 
 module.exports = {
    User,
